3fffbfd3ebe758189b5dc6115c738131ddfd7e5f
nchiz <nchiz96@gmail.com>
2021-10-20 15:57:49 +0300

    version0.2(add_html)

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+class Node{
+    constructor(data,next=null){
+        this.data = data
+        this.next = next
+    }
+}
 class LinkedList{
     constructor(){
         this.head = null      ///first element
         this.tail = null      /// last element
     }
     append(data){       /// add element in the end      
-        const node ={data,next:null}
+        const node =new Node(data)
         
         if(this.tail){
             this.tail.next = node
@@ -15,15 +21,25 @@ class LinkedList{
         this.tail = node
     }
     prepend(data){
-        const node = {data, next: this.head}
+        const node = new Node(data, this.head)
         this.head = node
-        if(!this.tail){
-            this.tail=node
+        if(!this.tail) {
+            this.tail = node
         }
+    }
+    toArray() {
+        const output = []
+        let current = this.head
+
+        while(current) {
+            output.push(current)
+            current = current.next
+        }
+        return output
     }
 }
 const list = new LinkedList()
 list.append('23')
 list.append('42')
-list.prepend('1')
-list // 
+list.prepend('2')
+console.log(list.toArray())
